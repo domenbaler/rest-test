1599932b447fb9c43c982408274c0361bb046bb0
Domen Baler <domen.baler@gmail.com>
2021-12-15 21:02:09 +0100

    refactored test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -8,38 +8,48 @@ public class FirstTest{
 
     @Test
     public void test_get() {
-        given().
-                when().
-                get("http://localhost:8080/rest").
-                then().
-                assertThat().
-                    statusCode(200).
-                and().
-                    contentType(ContentType.JSON).
-                and().
-                    body("NumberOfSuccessfulCalls",equalTo("0")).
-                and().
-                    body("NumberOfUnsuccessfulCalls", equalTo("0"));
+        get("http://localhost:8080/rest").
+        then().
+        assertThat().
+            statusCode(200).
+        and().
+            contentType(ContentType.JSON).
+        and().
+            body("NumberOfSuccessfulCalls",equalTo("0")).
+        and().
+            body("NumberOfUnsuccessfulCalls", equalTo("0"));
     }
 
     @Test(dependsOnMethods="test_get")
     public void test_valid_param() {
-        given().
-                when().
-                post("http://localhost:8080/rest/4").
-                then().
-                assertThat().
-                statusCode(204);
+        post("http://localhost:8080/rest/4").
+        then().
+        assertThat().
+        statusCode(204);
     }
 
     @Test
     public void test_invalid_param() {
-        given().
-                when().
-                post("http://localhost:8080/rest/5").
-                then().
-                assertThat().
-                statusCode(400);
+        post("http://localhost:8080/rest/5").
+        then().
+        assertThat().
+        statusCode(400);
+    }
+
+    @Test(dependsOnMethods="test_valid_param")
+    public void test_get_after_post() throws InterruptedException {
+        Thread.sleep(5000);
+        get("http://localhost:8080/rest").
+        then().
+        assertThat().
+            statusCode(200).
+        and().
+            contentType(ContentType.JSON).
+        and().
+            body("NumberOfSuccessfulCalls",equalTo("4")).
+        and().
+            body("NumberOfUnsuccessfulCalls", equalTo("0")).
+        and().body(containsString("Kariera – RESULT"));
     }
 
 }
